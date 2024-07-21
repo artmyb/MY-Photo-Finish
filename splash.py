6ff79243c88b99b335a99f46f4d5151c1022b053
Murat Yaşar BASKIN <74744060+artmyb@users.noreply.github.com>
2024-07-22 00:26:04 +0300

    Add files via upload

---
+++ splash.py
@@ -0,0 +1,163 @@
+import base64
+
+with open('images/main.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('main_icon.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/splash.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('splash_image.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/generate_image_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('generate_image_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/update_image_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('update_image_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/preview_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('preview_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/apply_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('apply_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/import_video_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('import_video_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/import_video_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('import_video_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/export_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('export_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/export_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('export_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/add_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('add_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/remove_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('remove_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/add_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('add_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/remove_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('remove_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/import_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('import_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/import_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('import_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/add_instance_button.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('add_instance_button.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/add_instance_button_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('add_instance_button_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/video.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('video.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/play.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('play.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/pause.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('pause.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/stop.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('stop.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/play_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('play_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/pause_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('pause_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/stop_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('stop_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/video_l.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('video_l.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
+
+with open('images/video_l_w.png', 'rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+with open('video_l_w.py', 'w') as file:
+    file.write(f'image_data = """{encoded_string}"""')
